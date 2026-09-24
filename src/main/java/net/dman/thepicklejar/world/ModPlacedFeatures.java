7e58eaf5b55c2ed870e54a4b3d5dd203bb8c6856
D-man Hartey <dmanhartey@gmail.com>
2026-09-24 14:54:28 +0800

    THE JAR EXPANDS

--- src/main/java/net/dman/thepicklejar/world/ModPlacedFeatures.java
+++ src/main/java/net/dman/thepicklejar/world/ModPlacedFeatures.java
@@ -25,7 +25,7 @@ public class ModPlacedFeatures {
                 ModOrePlacement.modifiersWithCount(5, // Veins per Chunk
                         HeightRangePlacementModifier.uniform(YOffset.fixed(-80), YOffset.fixed(80))));
         register(context, CHUTNEY_DEPOSIT_PLACED_KEY, configuredFeatureRegistryEntryLookup.getOrThrow(ModConfiguredFeatures.CHUTNEY_DEPOSIT_KEY),
-                ModOrePlacement.modifiersWithCount(3, // Veins per Chunk
+                ModOrePlacement.modifiersWithCount(4, // Veins per Chunk
                         HeightRangePlacementModifier.uniform(YOffset.fixed(-80), YOffset.fixed(80))));
 
     }
